e258d08a7501d67ee4570bfe1d3d6caa62d6a87c
volkandogan <vo.dogan@gmail.com>
2022-11-16 14:00:13 +0300

    Styles edited

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,23 +1,10 @@
 import { StyleSheet, Text } from 'react-native'
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import Actions from '../redux/actions';
+import Actions from '../../redux/actions';
 import styled from 'styled-components/native';
-import { BriefCard } from '../components';
-
-const BriefContainer = styled.View`
-flex-direction: column;
-justify-content: center,
-align-items: center,
-margin-left:16px;
-margin-right:16px;
-`
-const Container = styled.View`
-display:flex;
-flex:1;
-background-color:#FFF;
-justify-content:center; 
-`
+import { BriefCard } from '../../components';
+import { Container, BriefContainer } from './Home.styles';
 
 const Home = () => {
     const dispatch = useDispatch();
---
+++ src/screens/home/Home.styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+const BriefContainer = styled.View`
+flex-direction: column;
+justify-content: center,
+align-items: center,
+margin-left:16px;
+margin-right:16px;
+`
+const Container = styled.View`
+display:flex;
+flex:1;
+background-color:#FFF;
+justify-content:center; 
+`
